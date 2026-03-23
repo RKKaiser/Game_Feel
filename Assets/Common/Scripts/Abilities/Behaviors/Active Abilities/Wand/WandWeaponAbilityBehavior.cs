ada0f2f2229eb125ddb829ec1017b45386929690
容克KaiserQWQ <1059657605@qq.com>
2026-03-23 14:08:10 +0800

    wand武器类变为鼠标瞄准

--- Assets/Common/Scripts/Abilities/Behaviors/Active Abilities/Wand/WandWeaponAbilityBehavior.cs
+++ Assets/Common/Scripts/Abilities/Behaviors/Active Abilities/Wand/WandWeaponAbilityBehavior.cs
@@ -4,6 +4,7 @@ using OctoberStudio.Pool;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem; // 添加新输入系统命名空间
 
 namespace OctoberStudio.Abilities
 {
@@ -17,14 +18,13 @@ namespace OctoberStudio.Abilities
         private PoolComponent<SimplePlayerProjectileBehavior> projectilePool;
         public List<SimplePlayerProjectileBehavior> projectiles = new List<SimplePlayerProjectileBehavior>();
 
-        IEasingCoroutine projectileCoroutine;
         Coroutine abilityCoroutine;
 
         private float AbilityCooldown => AbilityLevel.AbilityCooldown * PlayerBehavior.Player.CooldownMultiplier;
 
         private void Awake()
         {
-            projectilePool = new PoolComponent<SimplePlayerProjectileBehavior>("Wand Projectiule", ProjectilePrefab, 50);
+            projectilePool = new PoolComponent<SimplePlayerProjectileBehavior>("Wand Projectile", ProjectilePrefab, 50);
         }
 
         protected override void SetAbilityLevel(int stageId)
@@ -42,20 +42,13 @@ namespace OctoberStudio.Abilities
 
             while (true)
             {
-                while(lastTimeSpawned + AbilityCooldown < Time.time)
+                while (lastTimeSpawned + AbilityCooldown < Time.time)
                 {
                     var spawnTime = lastTimeSpawned + AbilityCooldown;
 
                     var projectile = projectilePool.GetEntity();
 
-                    var closestEnemy = StageController.EnemiesSpawner.GetClosestEnemy(PlayerBehavior.CenterPosition);
-
-                    var direction = Vector2.up;
-                    if(closestEnemy != null)
-                    {
-                        direction = closestEnemy.Center - PlayerBehavior.CenterPosition;
-                        direction.Normalize();
-                    }
+                    Vector2 direction = GetMouseDirection();
 
                     var aliveDuration = Time.time - spawnTime;
                     var position = PlayerBehavior.CenterPosition + direction * aliveDuration * AbilityLevel.ProjectileSpeed * PlayerBehavior.Player.ProjectileSpeedMultiplier;
@@ -78,6 +71,30 @@ namespace OctoberStudio.Abilities
             }
         }
 
+        private Vector2 GetMouseDirection()
+        {
+            // 使用新 Input System 获取鼠标位置
+            var mouse = Mouse.current;
+            if (mouse == null)
+            {
+                // 如果没有鼠标设备（例如在编辑器外），返回默认方向
+                return Vector2.up;
+            }
+
+            Vector2 mouseScreenPos = mouse.position.ReadValue();
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
+            mouseWorldPos.z = 0f;
+
+            Vector2 mousePos2D = new Vector2(mouseWorldPos.x, mouseWorldPos.y);
+            Vector2 direction = mousePos2D - PlayerBehavior.CenterPosition;
+
+            if (direction.sqrMagnitude < 0.001f)
+            {
+                return Vector2.up;
+            }
+            return direction.normalized;
+        }
+
         private void OnProjectileFinished(SimplePlayerProjectileBehavior projectile)
         {
             projectile.onFinished -= OnProjectileFinished;
@@ -87,8 +104,6 @@ namespace OctoberStudio.Abilities
 
         private void Disable()
         {
-            projectileCoroutine.StopIfExists();
-
             for (int i = 0; i < projectiles.Count; i++)
             {
                 projectiles[i].gameObject.SetActive(false);
@@ -96,7 +111,8 @@ namespace OctoberStudio.Abilities
 
             projectiles.Clear();
 
-            StopCoroutine(abilityCoroutine);
+            if (abilityCoroutine != null)
+                StopCoroutine(abilityCoroutine);
         }
 
         public override void Clear()
